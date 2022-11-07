a6c2109daec7a7cfaee82d4b0f70818892085b6e
Dorottya Ferencz <retyidoro@yahoo.com>
2022-11-07 12:10:22 +0200

    Commit from TeleportHQ

---
+++ src/components/component.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="component-container" v-bind:class="rootClassName">
+    <span>{{ text }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppComponent',
+  props: {
+    text: {
+      type: String,
+      default: 'Text',
+    },
+    rootClassName: String,
+  },
+}
+</script>
+
+<style scoped>
+.component-container {
+  width: 372px;
+  height: 184px;
+  display: flex;
+  position: relative;
+  align-items: flex-start;
+  border-color: var(--dl-color-gray-black);
+  border-width: 1px;
+  flex-direction: column;
+}
+.component-root-class-name {
+  width: 0px;
+}
+@media(max-width: 479px) {
+  .component-container {
+    width: 320px;
+    height: 1074px;
+  }
+}
+</style>
---
+++ src/views/home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="home-container">
+    <div class="home-container1 bg-purple-400"></div>
+    <h1 class="bg-teal-400">Heading</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  metaInfo: {
+    title: 'Landing Page',
+    meta: [
+      {
+        property: 'og:title',
+        content: 'Landing Page',
+      },
+    ],
+  },
+}
+</script>
+
+<style scoped>
+.home-container {
+  width: 100%;
+  height: auto;
+  display: flex;
+  min-height: 100vh;
+  align-items: center;
+  flex-direction: column;
+}
+.home-container1 {
+  flex: 0 0 auto;
+  width: 200px;
+  border: 2px dashed rgba(120, 120, 120, 0.4);
+  height: 100px;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+}
+@media(max-width: 1600px) {
+  .home-container {
+    align-items: flex-end;
+  }
+}
+</style>
